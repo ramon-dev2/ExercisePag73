2a71e5cf41c83c794f55e3118f9053c91cf8e6a0
ramon <juanramonfernandez99@gmail.com>
2026-05-26 14:32:57 -0700

    sum avarage

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int suma = 0;
+        double promedio;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            int numero = sc.nextInt();
+
+            suma = suma + numero;
+        }
+
+        promedio = suma / 10.0;
+
+        System.out.println("La suma es: " + suma);
+        System.out.println("El promedio es: " + promedio);
+        }
+    }
